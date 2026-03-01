9e8716d842c2d9594648b4a83f4ab9c567812daa
DESKTOP-9BCUL9P\Admin <duy>
2026-03-01 23:01:06 +0700

    sa

---
+++ back-end/signalR/signalR/Controllers/BotController.cs
@@ -0,0 +1,27 @@
+﻿using BinanceRSIAPI.Models;
+using BinanceRSIAPI.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BinanceRSIAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> BotControl([FromBody] BotControlRequestDto requestDto)
+        {
+            Console.WriteLine("Change bot status to: " + requestDto.IsWorking);
+            if (requestDto.IsWorking)
+            {
+                GlobalVariables.IsWorking = true;
+            } else
+            {
+                GlobalVariables.IsWorking = false;
+            }
+            GlobalVariables.IsNeedLimit = requestDto.IsNeedLimit;
+            GlobalVariables.MaximumOrders = requestDto.MaximumOrders;
+            return Ok();
+        }
+    }
+}
