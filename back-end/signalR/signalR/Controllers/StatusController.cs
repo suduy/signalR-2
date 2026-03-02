2a2077f537d53dcfcfd7e10dc69363413b0d676c
Duy.Su <duy.su@ptnglobalcorp.com>
2026-03-02 17:00:29 +0700

    add status controller

---
+++ back-end/signalR/signalR/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+
+namespace BinanceRSIAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Status ()
+        {
+            return Ok();
+        }
+    }
+}
